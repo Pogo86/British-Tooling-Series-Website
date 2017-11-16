da475fd5cbc850dd527e140fb726d75087897d44
Rich Ellinson <rich@notebookds.com>
2017-11-16 11:06:03 +0000

    Initial Upload

---
+++ web/index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>British Tooling Series 2017</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/home.css" rel="stylesheet" type="text/css">
+    <link href="css/gallery.css" rel="stylesheet" type="text/css">
+    <!-- Google Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+           
+  </head>
+<body style="padding-top: 70px">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    	<div class="navbar-header">
+	      		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+	      		<a class="navbar-brand" id="navLogo" href="#">British Tooling Series</a>
+          		<a class="navbar-brand" id="navLogoMobile" href="#">B.T.S.</a>
+          	</div>
+	    		<!-- Collect the nav links, forms, and other content for toggling -->
+	    	<div class="collapse navbar-collapse" id="topFixedNavbar1">
+	    		<ul class="nav navbar-nav navbar-right" id="navBar">
+					<li><a href="#about">About</a></li>
+        			<li class="dropdown-menu-right">
+       					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Events<span class="caret"></span></a>
+        				<ul class="dropdown-menu" role="menu">
+                			<li><a href="#events">- King Kong</a></li>
+                   			<li class="divider"></li>
+               				<li><a href="#events">- Rope Race</a></li>
+                   			<li class="divider"></li>
+               				<li><a href="#events">- Ice Factor</a></li>
+           				</ul>
+        			</li>
+       				<li><a href="#results">Results</a></li>
+   					<li><a href="#gallery">Gallery</a></li>
+					<li><a href="#Sponsors">Sponsors</a></li>
+					<li>
+       					<a href="https://www.facebook.com/BritishToolingSeries" target="_blank" id="socialHeadFB"></a>
+       					<a href="https://www.facebook.com/BritishToolingSeries" target="_blank" id="socialHeadFBText">Facebook</a>
+       				</li>
+       				<li>
+        				<a href="https://twitter.com/BritishTooling" target="_blank" id="socialHeadTwitter"> </a>
+            			<a href="https://twitter.com/BritishTooling" target="_blank" id="socialHeadTwitterText">Twitter</a>
+       				</li>
+        			<li>
+        				<a href="#" target="_blank" id="socialHeadInsta"> </a>
+						<a href="#" target="_blank" id="socialHeadInstaText">Instagram</a>
+       				</li>
+       			</ul>
+       		</div>
+	    	<!-- /.navbar-collapse -->
+      	</div>
+	</nav>
+	<!-- /.container-fluid -->
+	<section id="header">
+    <div class="container-fluid">
+    	<div class="headimg">
+  
+    	</div><!-- This Div is the Image at the top of the page -->
+    </div>
+    </section>
+    <section id="about">
+	<div class="container-fluid home"><!-- This Div is the About Section -->
+		<div class="container-fluid" id="homeTop">
+        	<h1>About the British Tooling Series</h1>
+        	<h3>Climbing Walls + Axes = Crazy Competitions</h3>
+    	</div>
+    	<div class="container-fluid" id="homeAlt">
+			<div class="row">
+    			<div class="col-md-6">
+    				<p>British Tooling is a series of climbing competitions held at UK Climbing walls in the lead up to the Scottish winter climbing season. It allows participants to hone their skills and strength while competing amongst some of the sport’s top climbers.</p>
+        			<h3>Our Mission</h3>
+        			<ul>
+        				<li>To promote and encourage a raise in technical standard of drytooling and mixed climbing through competition.</li>
+            			<li>To introduce the activity to youth and new climbers.</li>
+            			<li>To encourage British climbers to compete in the UIAA Ice World Cup</li>
+            			<li>To work towards holding a UIAA World Cup event in British.</li>
+        			</ul>
+    			</div>
+    			<div class="col-md-6" align="center"><img src="images/aboutImg.jpg" alt="" class="img-responsive aboutImg" title=""/></div>
+    		</div>
+  		</div>
+  	</div>
+  	</section>
+  	<div class="container-fluid homeMid">
+  		<div class="home">
+  		<section id="events">
+        	<h1>Events</h1>
+        	<h2>Round 1 - King Kong - 28<sup>th</sup> October</h2>
+        	<h2>Round 2 - Rope Race - 11<sup>th</sup> November</h2>
+          	<h2>Round 3 - Ice Factor - 25<sup>th</sup> November</h2>
+		</section>
+        </div>
+    </div>
+	<div class="container-fluid home">
+		<h1>Results</h1>
+		<h2>Round 1 - King Kong</h2>
+		<section id="results">
+			<div class="col-md-6 col-sm-12">
+				<h3>Women</h3>
+				<table width="100%" border="1">
+					<tbody>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Score</th>
+						</tr>
+						<tr>
+							<td>Caitlin Connps</td>
+							<td>600</td>
+						</tr>
+						<tr>
+							<td>Anna Wells</td>
+							<td>600</td>
+						</tr>
+						<tr>
+							<td>Emma Powell</td>
+							<td>530</td>
+						</tr>
+						<tr>
+							<td>Laura Illingworth</td>
+							<td>110</td>
+						</tr>
+						<tr>
+							<td>Mary Holden</td>
+							<td>70</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<h3>Men</h3>
+				<table width="100%" border="1">
+					<tbody>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Score</th>
+						</tr>
+						<tr>
+							<td>Joe Saunders</td>
+							<td>600</td>
+						</tr>
+						<tr>
+							<td>Tim Millen</td>
+							<td>600</td>
+						</tr>
+						<tr>
+							<td>Charlie Booth</td>
+							<td>600</td>
+						</tr>
+						<tr>
+							<td>Will Woodhead</td>
+							<td>590</td>
+						</tr>
+						<tr>
+							<td>Jonathan Banner</td>
+							<td>580</td>
+						</tr>
+						<tr>
+							<td>Tim Miller</td>
+							<td>550</td>
+						</tr>
+						<tr>
+							<td>Simon Ward</td>
+							<td>420</td>
+						</tr>
+						<tr>
+							<td>Anthony Roberts</td>
+							<td>390</td>
+						</tr>
+						<tr>
+							<td>Rory Tunnah</td>
+							<td>340</td>
+						</tr>
+						<tr>
+							<td>Tony Walker</td>
+							<td>330</td>
+						</tr>
+						<tr>
+							<td>Lewis Simmonds</td>
+							<td>280</td>
+						</tr>
+						<tr>
+							<td>Dave Barker</td>
+							<td>260</td>
+						</tr>
+						<tr>
+							<td>Tom Armstrong</td>
+							<td>250</td>
+						</tr>
+						<tr>
+							<td>Adam McCudden</td>
+							<td>210</td>
+						</tr>
+						<tr>
+							<td>Ged Farmer</td>
+							<td>190</td>
+						</tr>
+						<tr>
+							<td>George Hayon</td>
+							<td>190</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		<h2>Round 2 - Rope Race</h2>
+			<div class="col-md-6 col-sm-12">
+				<h3>Women</h3>
+				<table width="100%" border="1">
+					<tbody>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Score</th>
+						</tr>
+						<tr>
+							<td>Caitlin Connps</td>
+							<td>540</td>
+						</tr>
+						<tr>
+							<td>Sophie Cheng</td>
+							<td>340</td>
+						</tr>
+						<tr>
+							<td>Laura Illingworth</td>
+							<td>230</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<h3>Men</h3>
+				<table width="100%" border="1">
+					<tbody>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Score</th>
+						</tr>
+						<tr>
+							<td>Will Woodhead</td>
+							<td>750</td>
+						</tr>
+						<tr>
+							<td>Simon Ward</td>
+							<td>740</td>
+						</tr>
+						<tr>
+							<td>Joe Saunders</td>
+							<td>740</td>
+						</tr>
+						<tr>
+							<td>Adam Wilson</td>
+							<td>740</td>
+						</tr>
+						<tr>
+							<td>Dave Clarke</td>
+							<td>600</td>
+						</tr>
+						<tr>
+							<td>Brett Ffitch</td>
+							<td>580</td>
+						</tr>
+						<tr>
+							<td>Neil Blake</td>
+							<td>490</td>
+						</tr>
+						<tr>
+							<td>Dave Barker</td>
+							<td>450</td>
+						</tr>
+						<tr>
+							<td>Ciaran</td>
+							<td>350</td>
+						</tr>
+						<tr>
+							<td>Tom Armstrong</td>
+							<td>330</td>
+						</tr>
+						<tr>
+							<td>Adam McCudden</td>
+							<td>310</td>
+						</tr>
+						<tr>
+							<td>Adam Underdown</td>
+							<td>290</td>
+						</tr>
+						<tr>
+							<td>Christoper Dale</td>
+							<td>290</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>		
+		</section>
+	</div>
+  	<div class="container-fluid homeLight" align="center">
+  		<div class="home">
+    	<section id="gallery">
+        	<h1>Gallery</h1>
+        </section>
+		</div>
+	</div>
+<!--
+	<section id="Sponsors">
+	<div class="container-fluid homeMid">
+  		<div class="home">		
+        	<h1>Sponsors</h1>
+           	<h2>To Be Announced</h2>
+            <div class="row">
+       			<div class="col-md-4" align="center">
+               		<a href="http://www.peakholds.com/" target="new"><h2>Peak Holds</h2></a>
+                    <a href="http://www.peakholds.com/"><img src="images/sponsors/Peak Single.png" alt="Peak Holds and Volumes" class="img-responsive" /></a>
+                    <h4>
+                    	<a href="http://www.peakholds.com/" target="new"><span class="website--url2">www.peakholds.com/</span></a>
+                    </h4>
+       		  	</div>
+       		  	<div class="col-md-4" align="center">
+                	<a href="http://dmmclimbing.com/" target="new">
+                	<h2>DMM</h2>
+                	</a>
+                    <a href="http://dmmclimbing.com/"><img src="images/sponsors/dmm.png" alt="DMM" class="img-thumbnail img-responsive" /></a>
+                    <h4>
+                    	<span class="website--url2"><a href="http://dmmclimbing.com/" target="new">www.dmmclimbing.com</a></span>
+                 	</h4>
+       		  	</div>       		  
+    		</div>     
+   		</div>
+	</div>
+	</section>
+-->
+</body>
+</html>
