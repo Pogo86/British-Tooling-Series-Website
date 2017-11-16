0b302486fe9722b45650e4e9a14c75e770a147f6
Rich Ellinson <rich@notebookds.com>
2017-11-16 13:04:23 +0000

    php Update

---
+++ web/headder.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>British Tooling Series 2017</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/home.css" rel="stylesheet" type="text/css">
+    <link href="css/gallery.css" rel="stylesheet" type="text/css">
+    <!-- Google Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/gallery.css" type="text/css">	
+	  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+           
+  </head>
+<body style="padding-top: 70px">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    	<div class="navbar-header">
+	      		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+	      		<a class="navbar-brand" id="navLogo" href="#">British Tooling Series</a>
+          		<a class="navbar-brand" id="navLogoMobile" href="#">B.T.S.</a>
+          	</div>
+	    		<!-- Collect the nav links, forms, and other content for toggling -->
+	    	<div class="collapse navbar-collapse" id="topFixedNavbar1">
+	    		<ul class="nav navbar-nav navbar-right" id="navBar">
+					<li><a href="#about">About</a></li>
+        			<li class="dropdown-menu-right">
+       					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Events<span class="caret"></span></a>
+        				<ul class="dropdown-menu" role="menu">
+                			<li><a href="#events">- King Kong</a></li>
+                   			<li class="divider"></li>
+               				<li><a href="#events">- Rope Race</a></li>
+                   			<li class="divider"></li>
+               				<li><a href="#events">- Ice Factor</a></li>
+           				</ul>
+        			</li>
+       				<li><a href="#results">Results</a></li>
+   					<li><a href="#gallery">Gallery</a></li>
+					<li><a href="#Sponsors">Sponsors</a></li>
+					<li>
+       					<a href="https://www.facebook.com/BritishToolingSeries" target="_blank" id="socialHeadFB"></a>
+       					<a href="https://www.facebook.com/BritishToolingSeries" target="_blank" id="socialHeadFBText">Facebook</a>
+       				</li>
+       				<li>
+        				<a href="https://twitter.com/BritishTooling" target="_blank" id="socialHeadTwitter"> </a>
+            			<a href="https://twitter.com/BritishTooling" target="_blank" id="socialHeadTwitterText">Twitter</a>
+       				</li>
+        			<li>
+        				<a href="#" target="_blank" id="socialHeadInsta"> </a>
+						<a href="#" target="_blank" id="socialHeadInstaText">Instagram</a>
+       				</li>
+       			</ul>
+       		</div>
+	    	<!-- /.navbar-collapse -->
+      	</div>
+	</nav>
+	<!-- /.container-fluid -->
+	<section id="header">
+    <div class="container-fluid">
+    	<div class="headimg">
+  
+    	</div><!-- This Div is the Image at the top of the page -->
+    </div>
+    </section>
---
+++ web/results.php
@@ -0,0 +1,197 @@
+<section id="results">	
+	<h1>Results</h1>
+	<h2>Round 1 - King Kong</h2>
+	<div class="col-md-6 col-sm-12">
+		<h3>Women</h3>
+		<table width="100%" border="1">
+			<tbody>
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Score</th>
+				</tr>
+				<tr>
+					<td>Caitlin Connps</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Anna Wells</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Emma Powell</td>
+					<td>530</td>
+				</tr>
+				<tr>
+					<td>Laura Illingworth</td>
+					<td>110</td>
+				</tr>
+				<tr>
+					<td>Mary Holden</td>
+					<td>70</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-6 col-sm-12">
+		<h3>Men</h3>
+		<table width="100%" border="1">
+			<tbody>
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Score</th>
+				</tr>
+				<tr>
+					<td>Joe Saunders</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Tim Millen</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Charlie Booth</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Will Woodhead</td>
+					<td>590</td>
+				</tr>
+				<tr>
+					<td>Jonathan Banner</td>
+					<td>580</td>
+				</tr>
+				<tr>
+					<td>Tim Miller</td>
+					<td>550</td>
+				</tr>
+				<tr>
+					<td>Simon Ward</td>
+					<td>420</td>
+				</tr>
+				<tr>
+					<td>Anthony Roberts</td>
+					<td>390</td>
+				</tr>
+				<tr>
+					<td>Rory Tunnah</td>
+					<td>340</td>
+				</tr>
+				<tr>
+					<td>Tony Walker</td>
+					<td>330</td>
+				</tr>
+				<tr>
+					<td>Lewis Simmonds</td>
+					<td>280</td>
+				</tr>
+				<tr>
+					<td>Dave Barker</td>
+					<td>260</td>
+				</tr>
+				<tr>
+					<td>Tom Armstrong</td>
+					<td>250</td>
+				</tr>
+				<tr>
+					<td>Adam McCudden</td>
+					<td>210</td>
+				</tr>
+				<tr>
+					<td>Ged Farmer</td>
+					<td>190</td>
+				</tr>
+				<tr>
+					<td>George Hayon</td>
+					<td>190</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<h2>Round 2 - Rope Race</h2>
+	<div class="col-md-6 col-sm-12">
+		<h3>Women</h3>
+		<table width="100%" border="1">
+			<tbody>
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Score</th>
+				</tr>
+				<tr>
+					<td>Caitlin Connps</td>
+					<td>540</td>
+				</tr>
+				<tr>
+					<td>Sophie Cheng</td>
+					<td>340</td>
+				</tr>
+				<tr>
+					<td>Laura Illingworth</td>
+					<td>230</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-6 col-sm-12">
+		<h3>Men</h3>
+		<table width="100%" border="1">
+			<tbody>
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Score</th>
+				</tr>
+				<tr>
+					<td>Will Woodhead</td>
+					<td>750</td>
+				</tr>
+				<tr>
+					<td>Simon Ward</td>
+					<td>740</td>
+				</tr>
+				<tr>
+					<td>Joe Saunders</td>
+					<td>740</td>
+				</tr>
+				<tr>
+					<td>Adam Wilson</td>
+					<td>740</td>
+				</tr>
+				<tr>
+					<td>Dave Clarke</td>
+					<td>600</td>
+				</tr>
+				<tr>
+					<td>Brett Ffitch</td>
+					<td>580</td>
+				</tr>
+				<tr>
+					<td>Neil Blake</td>
+					<td>490</td>
+				</tr>
+				<tr>
+					<td>Dave Barker</td>
+					<td>450</td>
+				</tr>
+				<tr>
+					<td>Ciaran</td>
+					<td>350</td>
+				</tr>
+				<tr>
+					<td>Tom Armstrong</td>
+					<td>330</td>
+				</tr>
+				<tr>
+					<td>Adam McCudden</td>
+					<td>310</td>
+				</tr>
+				<tr>
+					<td>Adam Underdown</td>
+					<td>290</td>
+				</tr>
+				<tr>
+					<td>Christoper Dale</td>
+					<td>290</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</section>
